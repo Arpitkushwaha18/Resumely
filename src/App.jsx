85cc1b8e6b9da32d01ae19e637c637ece3433b69
Arpit Kushwaha <arpitkushwaha@Arpits-MacBook-Air.local>
2026-06-02 11:14:30 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import BuilderPage from "./pages/BuilderPage.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 
@@ -9,6 +10,7 @@ export default function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/builder" element={<BuilderPage />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
